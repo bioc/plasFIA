a66627ec95a721a12b97128f54745dff8632ecf7
Lori Shepherd <lori.shepherd@roswellpark.org>
2021-03-30 13:30:49 -0400

    undeprecate

--- R/zzz.R
+++ R/zzz.R
@@ -8,9 +8,3 @@
      plasSet@path<-file.path(find.package("plasFIA"), "mzML")
      save(plasSet,file=pfile)
 }
-.onAttach <- function(libname, pkgname) {
-    msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.14")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
-}
